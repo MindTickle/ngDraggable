1d069962d2ac0aa346ee947be00a206ddb493d71
Kritivasas Shukla <kritivasas.shukla@mindtickle.com>
2018-11-14 02:51:06 -0500

    stopping propagation

--- ngDraggable.js
+++ ngDraggable.js
@@ -209,8 +209,8 @@ angular.module("ngDraggable", [])
                     if (_centerAnchor) {
                         // _tx = _mx - element.centerX - _dragOffset.left;
                         // _ty = _my - element.centerY - _dragOffset.top;
-                        _tx = _mx - _dragOffset.left;
-                        _ty = _my - _dragOffset.top;
+                        _tx = _mx;
+                        _ty = _my;
                     } else {
                         _tx = _mx - _mrx - _dragOffset.left;
                         _ty = _my - _mry - _dragOffset.top;
